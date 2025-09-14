2e7f353b6e2df5a4650694136dc643f03fa208c4
Quan Dang <18520339@gm.uit.edu.vn>
2025-09-15 08:52:26 +1000

    Refactor TemporalDeformableDetrForObjectDetection to use DeformableDetrModel and update related components for improved clarity and functionality

--- deformable_detr/__init__.py
+++ deformable_detr/__init__.py
@@ -1,1 +1,2 @@
-from model import TemporalDeformableDetrModel
+from .attention import TemporalMSDA
+from .model import TemporalDeformableDetrModel
--- pdvc.py
+++ pdvc.py
@@ -11,15 +11,15 @@ from transformers.models.deformable_detr.modeling_deformable_detr import (
     DeformableDetrObjectDetectionOutput,
     inverse_sigmoid
 )
-from deformable_detr import TemporalDeformableDetrModel
-from loss import TemporalDeformableDetrForObjectDetectionLoss
+from deformable_detr import DeformableDetrModel
+from loss import DeformableDetrForObjectDetectionLoss
 from utils import ensure_cw_format
 
 
-class TemporalDeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
+class DeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
     ''' Temporal Deformable DETR for 1D event localization.
     
-    Temporal detection head on top of TemporalDeformableDetrModel.
+    Temporal detection head on top of DeformableDetrModel.
     - class head: (d_model -> num_classes) with last class 'no-object'
     - bbox head: (d_model -> 2) predicts (center, length) normalized to [0,1]
     We do NOT use iterative box refinement (with_box_refine=False) to keep length independent of reference.
@@ -28,7 +28,7 @@ class TemporalDeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
     - pixel_values: (B, C, T)
     - pixel_mask:   (B, T) where 1=valid, 0=pad
     - labels: list of dicts per batch item with {
-        'class_labels': LongTensor (N_i, )  foreground classes (no "no-object" here)
+        'class_labels': LongTensor (N_i, )  foreground classes (no 'no-object' here)
         'boxes': FloatTensor (N_i, 2)       (center, width) normalized to [0,1]
     }
     '''
@@ -37,11 +37,11 @@ class TemporalDeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
     
     def __init__(self, config: DeformableDetrConfig, captioner, temporal_channels: Optional[List[int]] = None):
         super().__init__(config)
-        self.model = TemporalDeformableDetrModel(config, temporal_channels=temporal_channels) # Deformable DETR encoder-decoder model
+        self.transformer = DeformableDetrModel(config, temporal_channels=temporal_channels) # Deformable DETR encoder-decoder model
         
         # Detection heads on top: class + 2D temporal box (center, width)
-        self.count_embed = nn.Linear(config.d_model, self.config.num_queries + 1)  # Predict count of events in [0, num_queries]; last index for 0 events
-        self.class_embed = nn.Linear(config.d_model, config.num_labels)  # num_labels should include background
+        self.count_embed = nn.Linear(config.d_model, config.num_queries + 1)  # Predict count of events in [0, num_queries]; last index for 0 events
+        self.class_embed = nn.Linear(config.d_model, config.num_labels)  # num of foreground classes, no 'no-object' here
         self.bbox_embed = DeformableDetrMLPPredictionHead(input_dim=config.d_model, hidden_dim=config.d_model, output_dim=2, num_layers=3)
         self.captioner = captioner
         
@@ -54,25 +54,24 @@ class TemporalDeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
         if config.with_box_refine:
             self.count_embed = nn.ModuleList([deepcopy(self.count_embed) for _ in range(num_pred)])
             self.class_embed = nn.ModuleList([deepcopy(self.class_embed) for _ in range(num_pred)])
-            self.bbox_embed = nn.ModuleList([deepcopy(self.bbox_embed) for _ in range(num_pred)])
-            self.captioner = nn.ModuleList([deepcopy(self.captioner) for _ in range(num_pred)])
-            self.model.decoder.bbox_embed = self.bbox_embed # Hack implementation for iterative bounding box refinement
+            self.bbox_embed  = nn.ModuleList([deepcopy(self.bbox_embed)  for _ in range(num_pred)])
+            self.captioner   = nn.ModuleList([deepcopy(self.captioner)   for _ in range(num_pred)])
+            self.transformer.decoder.bbox_embed = self.bbox_embed # Hack implementation for iterative bounding box refinement
         else:
             nn.init.constant_(self.bbox_embed.layers[-1].bias.data[1:], -2)
             self.count_embed = nn.ModuleList([self.count_embed for _ in range(num_pred)])
             self.class_embed = nn.ModuleList([self.class_embed for _ in range(num_pred)])
-            self.bbox_embed = nn.ModuleList([self.bbox_embed for _ in range(num_pred)])
-            self.captioner = nn.ModuleList([self.captioner for _ in range(num_pred)])
-            self.model.decoder.bbox_embed = None
+            self.bbox_embed  = nn.ModuleList([self.bbox_embed  for _ in range(num_pred)])
+            self.captioner   = nn.ModuleList([self.captioner   for _ in range(num_pred)])
+            self.transformer.decoder.bbox_embed = None
             
-        self.loss_function = TemporalDeformableDetrForObjectDetectionLoss
+        # self.loss_function = DeformableDetrForObjectDetectionLoss
         self.post_init()
 
 
     def forward(
-        self,
-        pixel_values: torch.FloatTensor, # (B,C,T)
-        pixel_mask: Optional[torch.LongTensor] = None, # (B,T)
+        self, pixel_values: torch.FloatTensor, captions: torch.LongTensor, 
+        pixel_mask: Optional[torch.LongTensor] = None,
         decoder_attention_mask: Optional[torch.FloatTensor] = None,
         encoder_outputs: Optional[torch.FloatTensor] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
@@ -84,8 +83,8 @@ class TemporalDeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
     ) -> Union[tuple[torch.FloatTensor], DeformableDetrObjectDetectionOutput]:
         return_dict = return_dict if return_dict is not None else self.config.use_return_dic
 
-        # First, sent images through DETR base model to obtain encoder + decoder outputs
-        outputs = self.model(
+        # Send images through DETR base model to obtain encoder + decoder outputs
+        outputs = self.transformer(
             pixel_values,
             pixel_mask=pixel_mask,
             decoder_attention_mask=decoder_attention_mask,
@@ -97,7 +96,7 @@ class TemporalDeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
             return_dict=return_dict,
         )
 
-        # Decoder intermediate states: shape (B, L, Q, D) -> we need per-layer lists
+        # Decoder intermediate states: shape (B, L, Q, D) -> we need per-layer lists (D is the d_model)
         hidden_states = outputs.intermediate_hidden_states if return_dict else outputs[2]        # (B, L, Q, D)
         init_reference = outputs.init_reference_points if return_dict else outputs[0]            # (B, Q, 2)
         inter_references = outputs.intermediate_reference_points if return_dict else outputs[3]  # (B, L, Q, 2)
@@ -107,63 +106,53 @@ class TemporalDeformableDetrForObjectDetection(DeformableDetrPreTrainedModel):
         outputs_captions_probs, outputs_captions_seq = [], []
         
         for level in range(hidden_states.shape[1]):
+            lvl_hidden_states = hidden_states[:, level]  # (B, Q, D)
+            
             # Count head: (B, num_queries, num_queries+1) logits for 0 to num_queries events
-            outputs_count = self.count_embed[level](
-                # Max is to get the most confident prediction across queries
-                torch.max(hidden_states[:, level], dim=1, keepdim=False).values
-            )
+            # Max is to get the most confident prediction across queries
+            outputs_count = self.count_embed[level](torch.max(lvl_hidden_states, dim=1, keepdim=False).values)
             outputs_counts.append(outputs_count)
             
             # Class head: (B, num_queries, num_classes) logits
-            outputs_class = self.class_embed[level](hidden_states[:, level])  # (B, Q, C)
+            outputs_class = self.class_embed[level](lvl_hidden_states)  # (B, Q, C)
             outputs_classes.append(outputs_class)
             
             # Box head: (B, num_queries, 2) (center, width) in [0,1]
             reference = init_reference if level == 0 else inter_references[:, level - 1] # (B, Q, 2) if level != 0, we 
             reference = inverse_sigmoid(reference)                                       # (B, Q, 2)
-            delta_bbox = self.bbox_embed[level](hidden_states[:, level])                 # (B, Q, 2)
-            delta_bbox[..., :2] += reference  # Consistent with HF logic for 2D references
-            # delta_bbox[..., 0] += reference[..., 0] # Refine only the center. Leave length independent of reference
+            delta_bbox = self.bbox_embed[level](lvl_hidden_states)                       # (B, Q, 2)
+            if reference.shape[-1] == 2: delta_bbox += reference
+            elif reference.shape[-1] == 1: delta_bbox[..., :1] += reference
+            # elif reference.shape[-1] == 1: delta_bbox[..., :2] += reference
             outputs_coords.append(delta_bbox.sigmoid()) # (B, Q, 2) in (center,width) normalized [0,1]
 
-            # Caption head: (B, num_queries, max_len, vocab_size) logits
-            if level != hidden_states.shape[1] - 1: # No captioning at the last layer
-                cap_probs, seq = self.captioner[level](
-                    hidden_states[:, level], labels, reference, others, 
-                    'teacher_forcing' if self.training else 'greedy'
-                ) # (B, Q, max_len, vocab_size), (B, Q, max_len)
-                outputs_captions_probs.append(cap_probs)
-                outputs_captions_seq.append(seq)
-
-        outputs_counts = torch.stack(outputs_counts)  # (L, B, Q, num_queries+1)
-        outputs_class = torch.stack(outputs_classes)  # (L, B, Q, C)
-        outputs_coord = torch.stack(outputs_coords)   # (L, B, Q, 2)
-        logits = outputs_class[-1]                    # (B, Q, C)
-        pred_boxes = outputs_coord[-1]                # (B, Q, 2) (center,width) normalized
-        pred_counts = outputs_counts[-1]              # (B, Q, num_queries+1)
+        outputs_counts = torch.stack(outputs_counts)    # (L, B, Q, num_queries+1)
+        outputs_classes = torch.stack(outputs_classes)  # (L, B, Q, C)
+        outputs_coords = torch.stack(outputs_coords)    # (L, B, Q, 2)
+        logits = outputs_classes[-1]                    # (B, Q, C)
+        pred_boxes = outputs_coords[-1]                 # (B, Q, 2) (center,width) normalized
+        pred_counts = outputs_counts[-1]                # (B, Q, num_queries + 1)
 
         loss, loss_dict, auxiliary_outputs = None, None, None
         if labels is not None: # Training mode
-            fixed_targets: List[Dict[str, Tensor]] = [ 
+            labels: List[Dict[str, Tensor]] = [ 
                 # Ensure targets provide (center,width). If start/end (s<e & min>=0) provided, convert here
                 {'class_labels': t['class_labels'], 'boxes': ensure_cw_format(t['boxes'])}
                 for t in labels
             ]
             loss, loss_dict, auxiliary_outputs = self.loss_function(
-                logits, labels, self.device, pred_boxes, pred_counts,
-                self.config, outputs_class, outputs_coord,
+                labels, logits, pred_boxes, pred_counts,
+                outputs_classes, outputs_coords,
             )
 
         if not return_dict:
-            output = (logits, pred_boxes) + outputs
-            if auxiliary_outputs is not None: output += auxiliary_outputs
-            return ((loss, loss_dict) + output) if loss is not None else output
+            out = (logits, pred_boxes) + outputs
+            if auxiliary_outputs is not None: out += auxiliary_outputs
+            return ((loss, loss_dict) + out) if loss is not None else out
         
         return DeformableDetrObjectDetectionOutput(
-            loss=loss,
-            loss_dict=loss_dict,
-            logits=logits,
-            pred_boxes=pred_boxes,
+            loss=loss, loss_dict=loss_dict,
+            logits=logits, pred_boxes=pred_boxes,
             auxiliary_outputs=auxiliary_outputs,
             last_hidden_state=outputs.last_hidden_state,
             decoder_hidden_states=outputs.decoder_hidden_states,
