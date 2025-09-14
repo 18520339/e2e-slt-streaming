f3cb5a8c2f990333487408ca52725b20057f059a
Quan Dang <18520339@gm.uit.edu.vn>
2025-09-15 08:48:34 +1000

    Refine ImageLoss forward to return indices and turn the object detection loss into class format

--- loss.py
+++ loss.py
@@ -3,11 +3,19 @@ import torch.nn.functional as F
 from torch import nn, Tensor
 from typing import List, Tuple
 from scipy.optimize import linear_sum_assignment
-from transformers.loss.loss_for_object_detection import HungarianMatcher, ImageLoss, _set_aux_loss, sigmoid_focal_loss, 
+from transformers.loss.loss_for_object_detection import (
+    HungarianMatcher, ImageLoss, 
+    _set_aux_loss, sigmoid_focal_loss,
+    is_accelerate_available
+) 
 from utils import box_iou, generalized_box_iou, cw_to_se
 
-
-class TemporalHungarianMatcher(HungarianMatcher):
+if is_accelerate_available():
+    from accelerate import PartialState
+    from accelerate.utils import reduce
+    
+    
+class DeformableDetrHungarianMatcher(HungarianMatcher):
     '''
     Hungarian matcher for 1D temporal boxes/segments using SciPy's linear_sum_assignment with class, L1, and IoU costs.
     cost = class_cost + bbox_cost * L1(cw_pred, cw_tgt) + giou_cost * (1 - IoU(pred_se, tgt_se))
@@ -20,9 +28,9 @@ class TemporalHungarianMatcher(HungarianMatcher):
         - 'pred_boxes': (B, Q, 2) predicted (center, length) in [0,1]
           
         targets: list of length B, each dict has:
-        - 'class_labels': (N_i,) int64 in [0..C-1], foreground classes only (no no-object here)
+        - 'class_labels': (N_i,) int64 in [0..C-1], foreground classes only (no no-object here).
         - 'boxes':        (N_i, 2) in [0,1], (center,width) or (start,end). This matcher expects (center,width).
-        Returns: list of (predicted indices, target indices) per batch element
+        Returns: list of (predicted indices, target indices) per batch element.
         '''
         batch_size, num_queries = outputs['logits'].shape[:2]
         out_prob = outputs['logits'].flatten(0, 1).sigmoid()
@@ -51,7 +59,7 @@ class TemporalHungarianMatcher(HungarianMatcher):
         return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 
 
-class TemporalDeformableDetrImageLoss(ImageLoss):
+class DeformableDetrImageLoss(ImageLoss):
     ''' This class computes the loss for DETR. The process happens in 2 steps:
     1) We compute Hungarian assignment between ground truth boxes and the outputs of the model
     2) We supervise each pair of matched ground-truth / prediction (supervise class and box)
@@ -135,34 +143,72 @@ class TemporalDeformableDetrImageLoss(ImageLoss):
             'loss_self_iou': self_iou
         }
         
-        
-def TemporalDeformableDetrForObjectDetectionLoss(logits, labels, device, pred_boxes, pred_counts, config, outputs_class=None, outputs_coord=None, **kwargs):
-    matcher = TemporalHungarianMatcher(class_cost=config.class_cost, bbox_cost=config.bbox_cost, giou_cost=config.giou_cost)
-    criterion = TemporalDeformableDetrImageLoss(
-        matcher=matcher,
-        num_classes=config.num_labels,
-        focal_alpha=config.focal_alpha,
-        losses=['labels', 'boxes', 'cardinality'],
-    )
-    criterion.to(device)
     
-    # Compute the losses, based on outputs and labels
-    outputs = {'logits': logits, 'pred_boxes': pred_boxes, 'pred_counts': pred_counts}
-    auxiliary_outputs = None
-    
-    if config.auxiliary_loss:
-        auxiliary_outputs = _set_aux_loss(outputs_class, outputs_coord)
-        outputs['auxiliary_outputs'] = auxiliary_outputs
-    loss_dict = criterion(outputs, labels)
+    def forward(self, outputs, targets):
+        ''' This performs the loss computation.
+        Args:
+        outputs (`dict`, *optional*):
+            Dictionary of tensors, see the output specification of the model for the format.
+        targets (`list[dict]`, *optional*):
+            List of dicts, such that `len(targets) == batch_size`. The expected keys in each dict depends on the
+            losses applied, see each loss' doc.
+        '''
+        outputs_without_aux = {k: v for k, v in outputs.items() if k != 'auxiliary_outputs'}
+        last_indices = self.matcher(outputs_without_aux, targets) # Retrieve the matching between outputs of last layer and targets
 
-    # Compute total loss, as a weighted sum of the various losses
-    weight_dict = {'loss_ce': 1, 'loss_bbox': config.bbox_loss_coefficient}
-    weight_dict['loss_giou'] = config.giou_loss_coefficient
-    if config.auxiliary_loss:
-        aux_weight_dict = {}
-        for i in range(config.decoder_layers - 1):
-            aux_weight_dict.update({f'{k}_{i}': v for k, v in weight_dict.items()})
-        weight_dict.update(aux_weight_dict)
+        # Compute the average number of target boxes across all nodes, for normalization purposes
+        num_boxes = sum(len(t['class_labels']) for t in targets)
+        num_boxes = torch.as_tensor([num_boxes], dtype=torch.float, device=next(iter(outputs.values())).device)
+        world_size = 1
+        if is_accelerate_available():
+            if PartialState._shared_state != {}:
+                num_boxes = reduce(num_boxes)
+                world_size = PartialState().num_processes
+        num_boxes = torch.clamp(num_boxes / world_size, min=1).item()
+
+        # Compute all the requested losses
+        losses = {}
+        for loss in self.losses:
+            losses.update(self.get_loss(loss, outputs, targets, last_indices, num_boxes))
+
+        # In case of auxiliary losses, we repeat this process with the output of each intermediate layer.
+        if 'auxiliary_outputs' in outputs:
+            auxiliary_indices = []
+            for i, auxiliary_outputs in enumerate(outputs['auxiliary_outputs']):
+                indices = self.matcher(auxiliary_outputs, targets)
+                auxiliary_indices.append(indices)
+                for loss in self.losses:
+                    if loss == 'masks': continue # Intermediate masks losses are too costly to compute, we ignore them
+                    l_dict = self.get_loss(loss, auxiliary_outputs, targets, indices, num_boxes)
+                    losses.update({f'{k}_{i}': v for k, v in l_dict.items()})
+            return losses, last_indices, auxiliary_indices
+        return losses, last_indices
         
-    loss = sum(loss_dict[k] * weight_dict[k] for k in loss_dict if k in weight_dict)
-    return loss, loss_dict, auxiliary_outputs
+        
+class DeformableDetrForObjectDetectionLoss(DeformableDetrImageLoss):
+    def __init__(self, config):
+        super().__init__(
+            matcher=DeformableDetrHungarianMatcher(class_cost=config.class_cost, bbox_cost=config.bbox_cost, giou_cost=config.giou_cost),
+            num_classes=config.num_labels,
+            focal_alpha=config.focal_alpha,
+            losses=['labels', 'boxes', 'cardinality'],
+        )
+        self.config = config
+        self.weight_dict = {'loss_ce': 1, 'loss_bbox': config.bbox_loss_coefficient, 'loss_giou': config.giou_loss_coefficient}
+        self.auxiliary_outputs = None
+
+
+    def forward(self, labels, logits, pred_boxes, pred_counts, outputs_classes=None, outputs_coords=None, **kwargs):
+        outputs = {'logits': logits, 'pred_boxes': pred_boxes, 'pred_counts': pred_counts}
+        
+        if self.config.auxiliary_loss:
+            self.auxiliary_outputs = _set_aux_loss(outputs_classes, outputs_coords)
+            outputs['auxiliary_outputs'] = self.auxiliary_outputs
+            self.weight_dict.update({ # Weights for each decoder layer
+                f'{k}_{l}': v for l in range(self.config.decoder_layers - 1)
+                for k, v in self.weight_dict.items()
+            })
+        
+        loss_dict, last_indices, auxiliary_indices = super().forward(outputs, labels) # Compute the losses, based on outputs and labels
+        loss = sum(loss_dict[k] * self.weight_dict[k] for k in loss_dict if k in self.weight_dict)
+        return loss, last_indices, auxiliary_indices
